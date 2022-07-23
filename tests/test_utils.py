831f69df05a5efb80bd72cf03406a6805f9d1370
phisei <philipp.s.ascari@gmail.com>
2022-07-23 22:29:43 +0200

    clean commit

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,19 +1,23 @@
 import unittest
-
+import os
+import wandb
 import numpy as np
 import delphi.utils.tools as utils
+
+os.environ['WANDB_MODE'] = 'offline'
 
 
 class MyTestCase(unittest.TestCase):
 
     def test_ConvertFromWandbConfig_when_RequiredParamsMatchesTarget(self):
-
         test_wandb_cfg = {
             'lin_neurons1': 128,
             'lin_neurons2': 64,
             'lin_neurons3': 32,
-            '_wandb': {},
         }
+
+        wandb.init(config=test_wandb_cfg, entity='ml4ni', project='code-testing',
+                   dir='')
 
         target_cfg = {
             'lin_neurons': [128, 64, 32],
@@ -21,17 +25,18 @@ class MyTestCase(unittest.TestCase):
 
         required_params = ["lin_neurons"]
 
-        converted = utils.convert_wandb_config(test_wandb_cfg, required_params)
+        converted = utils.convert_wandb_config(wandb.config, required_params)
         self.assertDictEqual(target_cfg, converted)
 
     def test_ConvertFromWandbConfig_when_RequiredParamsDoesNotMatchTarget(self):
-
         test_wandb_cfg = {
             'lin_neurons1': 128,
             'lin_neurons2': 64,
             'lin_neurons3': 32,
-            '_wandb': {},
         }
+
+        wandb.init(config=test_wandb_cfg, entity='ml4ni', project='code-testing',
+                   dir='')
 
         target_cfg = {
             'lin_neurons': [128, 64, 32],
@@ -39,41 +44,18 @@ class MyTestCase(unittest.TestCase):
 
         required_params = ["lin_neurons", "dropout", "channels"]
 
-        converted = utils.convert_wandb_config(test_wandb_cfg, required_params)
-        self.assertDictEqual(target_cfg, converted)
-
-    def test_ConvertFromWandbConfig_with_ComplexConfig(self):
-
-        test_wandb_cfg = {
-            'channels1': 1,
-            'kernel_size': 3,
-            'lin_neurons1': 128,
-            'channels2': 8,
-            'lin_neurons3': 32,
-            'pooling_kernel': 2,
-            'lin_neurons2': 64,
-        }
-
-        target_cfg = {
-            'lin_neurons': [128, 64, 32],
-            'channels': [1, 8],
-            'pooling_kernel': 2,
-            'kernel_size': 3,
-        }
-
-        required_params = ["lin_neurons", "dropout", "channels", "pooling_kernel", "kernel_size"]
-
-        converted = utils.convert_wandb_config(test_wandb_cfg, required_params)
+        converted = utils.convert_wandb_config(wandb.config, required_params)
         self.assertDictEqual(target_cfg, converted)
 
     def test_ConvertFromWandbConfig_when_TestConfigNotOrdered(self):
-
         test_wandb_cfg = {
             'lin_neurons2': 64,
             'lin_neurons1': 128,
             'lin_neurons3': 32,
-            '_wandb': {},
         }
+
+        wandb.init(config=test_wandb_cfg, entity='ml4ni', project='code-testing',
+                   dir='')
 
         target_cfg = {
             'lin_neurons': [128, 64, 32],
@@ -81,7 +63,7 @@ class MyTestCase(unittest.TestCase):
 
         required_params = ["lin_neurons"]
 
-        converted = utils.convert_wandb_config(test_wandb_cfg, required_params)
+        converted = utils.convert_wandb_config(wandb.config, required_params)
         self.assertDictEqual(target_cfg, converted)
 
     def test_calculate_accuracy(self):
@@ -102,6 +84,11 @@ class MyTestCase(unittest.TestCase):
         func = utils.ToTensor()
         tensor = func([1, 2, 3, 4])
         self.assertIsInstance(tensor, torch.Tensor)
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        import shutil
+        shutil.rmtree("wandb", ignore_errors=True)
 
 
 if __name__ == '__main__':
